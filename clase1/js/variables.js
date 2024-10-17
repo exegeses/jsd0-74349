5460fa4603e8f2f4f3b6a19b4277db53c34556b5
Marcos <exegeses@gmail.com>
2024-10-17 10:07:07 -0300

    Definición de javascript | inserción de código, dot syntax | variables y constantes

---
+++ clase1/js/variables.js
@@ -0,0 +1,25 @@
+/*
+    Para declarar una variable en Java script 
+    utilizamos la palabra reservada 'let'
+*/
+let numero = 73;
+let frase = 'creación de variables en Javascript';
+
+/*
+    Para declarar una constante en Java script 
+    utilizamos la palabra reservada 'const'
+*/
+const PI = 3.141592;
+
+
+// Escribimos en el documento
+document.write(frase);
+document.write('<br>');
+
+// sobre escribimos el valor de la variable frase
+frase = 'hola mundo desde JavaScript';
+// PI = 56879; No se puede sobre escribir una constante
+
+// Escribimos en el documento
+document.write(frase);
+
